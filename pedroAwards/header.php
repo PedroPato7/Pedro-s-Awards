c08fcdc939b33ece094cac3274426ea54244cba8
Pedro Damann <141420135+PedroPato7@users.noreply.github.com>
2025-01-15 17:39:30 -0300

    Add files via upload

---
+++ pedroAwards/header.php
@@ -0,0 +1,14 @@
+<!-- header.php -->
+<header>
+    <div class="logo">
+        <a href="index.php">🐇 Pedro's Awards 🦆</a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="nominees.php">Nominados</a></li>
+            <li><a href="winners.php">Ganhadores</a></li>
+            <li><a href="contact.php">Contato</a></li>
+        </ul>
+    </nav>
+</header>
